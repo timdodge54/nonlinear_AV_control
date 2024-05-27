411a63eaf45563a880e48af3dce03fb793c6856d
Tim Dodge <tim.dodge64@gmail.com>
2024-05-26 21:30:33 -0600

    added render

--- nonliner_av_control/trajectoryEnv.py
+++ nonliner_av_control/trajectoryEnv.py
@@ -42,14 +42,13 @@ class TrajectoryEnv(gym.Env):
     def step(self, action):
         index = np.floor(self.t*100) + 1
         # Gather the desired values for the next waypoint
-        vr = self.vd[index]
-        wr = self.thetaddot[index]
         thetar = self.thetad[index]
         xr = self.xd[index]
         yr = self.yd[index]
         x = self.state[0]
         y = self.state[1]
         theta = self.state[2]
+        # this is not needed for the norm but i wanna and you cant stop me :P
         rotation_matrix: npt.NDArray[np.float64] = np.array([
         [np.cos(theta), np.sin(theta), 0],
         [-np.sin(theta), np.cos(theta), 0],
@@ -80,9 +79,32 @@ class TrajectoryEnv(gym.Env):
         self.state = np.array([new_x, new_y, new_theta], dtype=np.float32)
         return self.state, reward, done, {}
 
+    def render(self, mode='human'):
+        # render with arrow pointing in the direction of the third state
+        pygame.init()
+        screen = pygame.display.set_mode((self.window_size, self.window_size))
+        screen.fill((255, 255, 255))
+        x = self.state[0]
+        y = self.state[1]
+        theta = self.state[2]
+        x = int(x * self.window_size / 2000 + self.window_size / 2)
+        y = int(y * self.window_size / 2000 + self.window_size / 2)
+        theta = -theta
+        theta = int(theta * 180 / np.pi)
+        arrow = pygame.Surface((50, 50), pygame.SRCALPHA)
+        pygame.draw.polygon(arrow, (0, 0, 0), ((25, 0), (50, 50), (0, 50)))
+        arrow = pygame.transform.rotate(arrow, theta)
+        screen.blit(arrow, (x, y))
+        pygame.display.flip()
+        pygame.time.wait(100)
+        if mode == 'rgb_array':
+            data = pygame.surfarray.array3d(screen)
+            pygame.quit()
+            return data
+        return None
 
     def reset(self):
-        self.state = np.array([0., 0.], dtype=np.float32)
+        self.state = np.array([0., 0., 0.], dtype=np.float32)
         return self.state
     def close(self):
         pass
